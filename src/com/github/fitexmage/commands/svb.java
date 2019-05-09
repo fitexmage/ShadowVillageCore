c38a68a74f5fdbf132d4bfa4f4f5855dc6d7e6a2
Shengguang Bai <fitexmage@126.com>
2019-05-09 02:51:31 -0400

    Fix a bug.

--- src/com/github/fitexmage/commands/svb.java
+++ src/com/github/fitexmage/commands/svb.java
@@ -347,7 +347,7 @@ public class svb extends ShadowVillageCommand {
             if (itemInHand != null && itemInHand.getType() != Material.AIR) {
                 if (ShadowItem.isBounded(itemInHand)) {
                     Message.sendMessage(player, "物品已经被灵魂绑定了！");
-                } else if (EconomyUtil.economy.getBalance(player) >= 10) {
+                } else if (EconomyUtil.economy.getBalance(player) < 10) {
                     Message.sendMessage(player, "你的金钱不足！");
                 } else {
                     ItemMeta meta = itemInHand.getItemMeta();
